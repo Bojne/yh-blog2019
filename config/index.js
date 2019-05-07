0c1ff064fc031eb3495292c537e6342f7c4fa3d6
岳涵黃 <yuehanhuang@yuehanhuangde-MacBook-Pro.local>
2019-05-07 02:11:09 -0700

    change banner and stuff

--- config/index.js
+++ config/index.js
@@ -4,18 +4,18 @@ module.exports = {
   siteTitle: 'Yueh Han Blog', // Navigation and Site Title
   siteTitleAlt: 'Yueh Han Huang, Student at Minerva', // Alternative Site title for SEO
   siteTitleManifest: 'YuehHanBlog',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://yuehan.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Yueh Han is Minerva Student from Taipei, Taiwan.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  author: 'Yueh Han Huang', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@boj_ne', // Twitter Username - Optional
-  ogSiteName: 'minimal', // Facebook Site Name - Optional
+  ogSiteName: 'Yueh Han', // Facebook Site Name - Optional
   ogLanguage: 'en_US', // Facebook Language
   googleAnalyticsID: 'UA-47519312-6',
 
